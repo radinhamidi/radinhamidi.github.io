f655dace23a8584deb6ff889338403df43e05f77
radinhamidi <radin.h@gmail.com>
2025-07-08 04:46:24 +0000

    Deploying to gh-pages from @ radinhamidi/radinhamidi.github.io@c98a290d7024fa72e365567edfdb71613bc552b0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,51 +408,26 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-wisest-which-systematic-evidence-synthesis-is-best",
+          title: 'WISEST (WhIch Systematic Evidence Synthesis is besT)',
+          description: "AI-powered quality assessment of systematic reviews (2023-2025)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/10_wisest/";
+            },},{id: "projects-data-observatory",
+          title: 'Data Observatory',
+          description: "AI-enabled infrastructure for systematic data collection &amp; text analysis (2024-2025)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/11_data_observatory/";
+            },},{id: "projects-heterogeneous-graph-representation-learning",
+          title: 'Heterogeneous Graph Representation Learning',
+          description: "Neural architectures for dynamic heterogeneous graphs (2022-2024)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/12_heterogeneous_graph/";
+            },},{id: "projects-team-formation",
+          title: 'Team Formation',
+          description: "Graph search &amp; GNN models for expert team assembly (2019-2022)",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/13_team_formation/";
             },},{
         id: 'social-dblp',
         title: 'DBLP',
