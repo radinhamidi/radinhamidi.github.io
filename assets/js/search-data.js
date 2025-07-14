9bd721c389ac75b530027db5344045313f2ec7e6
radinhamidi <radin.h@gmail.com>
2025-07-14 10:55:26 +0000

    Deploying to gh-pages from @ radinhamidi/radinhamidi.github.io@463ff5c0b8d08663c4c5abdb94d927c3fb8c779c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -301,7 +301,7 @@ ninja.data = [{
         section: "Posts",
         handler: () => {
           
-            window.location.href = "/assets/pdf/example_pdf.pdf";
+            window.location.href = "/assets/pdf/radin_resume.pdf";
           
         },
       },{id: "post-a-post-with-diagrams",
